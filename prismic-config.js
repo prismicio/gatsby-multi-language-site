b713465049e6cef9530c70c4fd07ab2bd24c8000
Levi Gable <levimykel@gmail.com>
2020-05-29 12:56:57 +0200

    Update default repo name

--- prismic-config.js
+++ prismic-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // The repo name for your Prismic repository (found in the url of your repo: https://your-repo-name.prismic.io/)
-  prismicRepo: 'multi-language-example',
+  prismicRepo: 'your-repo-name',
 
   // Language configuration in your Prismic repository: Settings / Translations & locals
   defaultLanguage: 'en-us',
