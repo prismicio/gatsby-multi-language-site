4ae0c2198f390e32c52767ed5138a2370f434072
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-06-22 17:12:54 +0200

    installed gatsby-source-prismic

--- prismic-config.js
+++ prismic-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // The repo name for your Prismic repository (found in the url of your repo: https://your-repo-name.prismic.io/)
-  prismicRepo: 'your-repo-name',
+  prismicRepo: 'multi-language-example',
 
   // Language configuration in your Prismic repository: Settings / Translations & locals
   defaultLanguage: 'en-us',
