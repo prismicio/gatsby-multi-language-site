62cd25d5948927d64cf47c8d6e5215cbc573f283
MarcMcIntosh <marcmcintosh1987@gmail.com>
2020-06-25 12:05:35 +0200

    add apiEndpoint to prismic-config to work with prismic theme

--- prismic-config.js
+++ prismic-config.js
@@ -1,15 +1,20 @@
+
 module.exports = {
   // The repo name for your Prismic repository (found in the url of your repo: https://your-repo-name.prismic.io/)
   prismicRepo: 'multi-language-example',
 
+  // For prismic theme command <https://user-guides.prismic.io/en/articles/764581-how-to-create-a-theme-to-quickstart-your-projects-for-prismic>
+
+  apiEndpoint: 'https://multi-language-example.prismic.io/api',
+
   // Preview path for handling redirects.
   previewPath: '/preview',
 
-  // Release ID to build
+  // Release ID to build (requires access token)
   releaseID: 'XvIgmxIAACMAzSaL',
 
   // access token to access the release
-  // accessToken: '',
+  accessToken: '',
 
   buildRelease: process && process.env && process.env.GATSBY_CLOUD && process.env.NODE_ENV === 'development',
 
