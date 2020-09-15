e909eb46d4930b0b9e62c0c4c8d45d8f9f6fed9d
Marc McIntosh <marcmcintosh1987@gmail.com>
2020-09-15 16:56:54 +0200

    Update prismic-configuration.js
    update prismic-configuration

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,20 +1,22 @@
 
 module.exports = {
+  // Confguration for dev.
+  
   // The repo name for your Prismic repository (found in the url of your repo: https://your-repo-name.prismic.io/)
-  prismicRepo: 'https://test-gatsby-cloud-manual.wroom.io',
+  
+  // prismicRepo: 'repo-name', ie: multi-language-example
+  
 
   // For prismic theme command <https://user-guides.prismic.io/en/articles/764581-how-to-create-a-theme-to-quickstart-your-projects-for-prismic>
 
-  apiEndpoint: 'https://test-gatsby-cloud-manual.wroom.io/api',
-
   // Preview path for handling redirects.
-  previewPath: '/preview',
+  // previewPath: '/preview',
 
   // Release ID to build (requires access)
   // releaseID: 'X1jznxEAABsAxWPY',
 
   // access token to access the release
-  accessToken: "",
+  // accessToken: "",
 
   // Language configuration in your Prismic repository: Settings / Translations & locals
   defaultLanguage: 'en-gb',
