7a02470358725b91ef0a0691e0365235b355f741
PaulinaVPG <pvpg@live.com.mx>
2020-09-21 01:17:35 -0500

    correct prismiconfig

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,21 +1,17 @@
 
 module.exports = {
-  // Confguration for dev.
   
   // The repo name for your Prismic repository (found in the url of your repo: https://your-repo-name.prismic.io/)
   prismicRepo: 'your-repo-name',
   
+  // Preview path for handling redirects
+  // previewPath: '',
 
-//   // For prismic theme command <https://user-guides.prismic.io/en/articles/764581-how-to-create-a-theme-to-quickstart-your-projects-for-prismic>
+  // Release ID to build (requires access)
+  // releaseID: '',
 
-//   // Preview path for handling redirects.
-//   previewPath: '/preview',
-
-//   // Release ID to build (requires access)
-//   releaseID: 'XvIgmxIAACMAzSaL',
-
-//   // access token to access the release
-//   accessToken: "MC5YMkxDSmhBQUFDUUFnQVVu.Vwt0RHsbYGHvv73vv71EZu-_ve-_vU0M77-9fu-_vSDvv70CQO-_ve-_ve-_vUxl77-9JO-_vVs",
+  // access token to access the release
+  // accessToken: "",
 
   // Language configuration in your Prismic repository: Settings / Translations & locals
   defaultLanguage: 'en-us',
