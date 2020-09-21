e443ff0b42768bfaec33174f31b6e03246a7c116
Paulina Gavilán <32859437+PaulinaVPG@users.noreply.github.com>
2020-09-20 23:35:27 -0500

    Update prismic-configuration.js

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -3,22 +3,21 @@ module.exports = {
   // Confguration for dev.
   
   // The repo name for your Prismic repository (found in the url of your repo: https://your-repo-name.prismic.io/)
-  
-  prismicRepo: 'your-repo-name',
+  prismicRepo: 'multilanguage-examples',
   
 
   // For prismic theme command <https://user-guides.prismic.io/en/articles/764581-how-to-create-a-theme-to-quickstart-your-projects-for-prismic>
 
   // Preview path for handling redirects.
-//   previewPath: '',
+  previewPath: '/preview',
 
   // Release ID to build (requires access)
-//   releaseID: '',
+  releaseID: 'XvIgmxIAACMAzSaL',
 
   // access token to access the release
-//   accessToken: "",
+  accessToken: "MC5YMkxDSmhBQUFDUUFnQVVu.Vwt0RHsbYGHvv73vv71EZu-_ve-_vU0M77-9fu-_vSDvv70CQO-_ve-_ve-_vUxl77-9JO-_vVs",
 
   // Language configuration in your Prismic repository: Settings / Translations & locals
-  defaultLanguage: 'en-gb',
-  langs: ['en-gb', 'fr-fr'],
+  defaultLanguage: 'en-us',
+  langs: ['en-us', 'fr-fr'],
 }
