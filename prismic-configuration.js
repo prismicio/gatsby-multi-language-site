868327d92d9e9a8af37a8c09f2c162a11abcd6e0
Levi Gable <levimykel@gmail.com>
2022-09-13 17:41:03 +0200

    feat: add the Prismic demo repository

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -7,7 +7,7 @@ module.exports = {
   // your repository.
   //
   // Example: 'my-repo' if your Prismic repository URL is 'my-repo.prismic.io'.
-  prismicRepo: 'your-repo-name',
+  prismicRepo: 'multi-language-example',
 
   // The default language for content in your Prismic repository.
   defaultLanguage: 'en-us',
